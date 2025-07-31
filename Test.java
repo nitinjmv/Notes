d6db62781612f3937363b1168d8587a4bd5b6b3d
nitinjmv <nitinjmv@gmail.com>
2025-07-31 23:15:31 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,30 +1,27 @@
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.TrustSelfSignedStrategy;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.ssl.SSLContexts;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ssl.TrustStrategy;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 
 import javax.net.ssl.SSLContext;
 
-@Configuration
-public class RestTemplateConfig {
+@Bean
+public RestTemplate restTemplate() throws Exception {
+    TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
 
-    @Bean
-    public RestTemplate restTemplate() throws Exception {
-        // Build an SSLContext that trusts self-signed certificates
-        SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-                .build();
+    SSLContext sslContext = SSLContexts.custom()
+            .loadTrustMaterial(null, acceptingTrustStrategy)
+            .build();
 
-        HttpClient httpClient = HttpClients.custom()
-                .setSSLContext(sslContext)
-                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE) // Disable hostname verification
-                .build();
+    CloseableHttpClient httpClient = HttpClients.custom()
+            .setSSLContext(sslContext)
+            .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+            .build();
 
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
-    }
+    return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
 }
